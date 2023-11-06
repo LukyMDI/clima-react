d72d0d6074a0e818d9831ee119f985d22afb9dc1
LukyMDI <97931181+LukyMDI@users.noreply.github.com>
2023-11-06 19:30:45 -0300

    Feita estilização do site e ajustes
    - Ajustes nas variáveis de ambiente
    - Estilização com TailwindCSS
    - Alteração no recebimento dos dados da API

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 
 import axios from "axios";
 
+import "./app.css";
+
 function App() {
     const [climaData, setClimaData] = useState(null);
     const [climaToday, setClimaToday] = useState(null);
@@ -20,7 +22,7 @@ function App() {
     const getToday = async (latitude, longitude) => {
         try {
             const response = await axios.get(
-                `${process.env.REACT_APP_API_TODAY}lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`
+                `${process.env.REACT_APP_API_TODAY}lat=${latitude}&lon=${longitude}&units=metric&lang=pt_br&appid=${apiKey}`
             );
 
             console.log(response.data);
@@ -34,12 +36,28 @@ function App() {
     const forecastData = async (latitude, longitude) => {
         try {
             const response = await axios.get(
-                `${process.env.REACT_APP_API_URL}lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`
+                `${process.env.REACT_APP_API_URL}?lat=${latitude}&lon=${longitude}&units=metric&lang=pt_br&appid=${apiKey}`
             );
 
             console.log(response.data);
 
-            setClimaData(response.data);
+            const currentData = new Date().toDateString();
+
+            const uniqueDays = {};
+
+            response.data.list.forEach((item) => {
+                const date = new Date(item.dt * 1000).toDateString();
+
+                if (date !== currentData) {
+                    if (!uniqueDays[date]) {
+                        uniqueDays[date] = item;
+                    }
+                }
+            });
+
+            const uniqueHours = Object.values(uniqueDays);
+
+            setClimaData(uniqueHours);
         } catch (error) {
             console.error(
                 "Ocorreu um erro ao buscar a previsão do tempo:",
@@ -64,70 +82,85 @@ function App() {
         }
     };
 
-    const formatData = (timestamp) => {
-        const data = new Date(timestamp * 1000);
-        const options = {
-            weekday: "long",
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-        };
-        return data.toLocaleDateString("pt-br", options);
-    };
-
-    const formatHours = (timestamp) => {
-        const data = new Date(timestamp * 1000);
-        const hora = data.getHours().toString().padStart(2, "0");
-        const minuto = data.getMinutes().toString().padStart(2, "0");
-        return `${hora}h${minuto}min`;
-    };
-
     return (
-        <div className="bg-[url('/public/sky.jpg')] bg-cover h-screen w-screen">
-            <div className="container mx-auto border flex flex-col items-center h-full justify-center">
-                <h1 className="text-4xl mb-5">Clima</h1>
-                <form onSubmit={handleSubmitClima} className="">
-                    <input
-                        type="text"
-                        value={location}
-                        onChange={(e) => setLocation(e.target.value)}
-                        className="border border-solid border-black rounded px-1 py-0.5 "
-                    />
-                    <button
-                        type="submit"
-                        className="bg-sky-500 text-white px-2 py-0.5 m-1 rounded hover:bg-sky-300"
-                    >
-                        Obter Clima
-                    </button>
+        <div className="bg-[url('/public/sky.jpg')] bg-cover w-full h-screen">
+            <div className="container mx-auto flex flex-col items-center h-full justify-center">
+                <form onSubmit={handleSubmitClima} className="my-12">
+                    <h1 className="text-4xl mb-10 text-white font-bold text-center">
+                        Weather Sky
+                    </h1>
+                    <div className="flex flex-row gap-2">
+                        <input
+                            type="text"
+                            value={location}
+                            onChange={(e) => setLocation(e.target.value)}
+                            placeholder="Insira o local"
+                            className="rounded px-2 xl:px-5 py-0.5 focus:outline-none h-[35px] sm:w-[400px] xl:h-[50px] xl:w-[500px]"
+                        />
+                        <button
+                            type="submit"
+                            className="bg-sky-500 font-bold text-white px-2 py-0.5 rounded h-[35px] hover:bg-sky-300 xl:h-[50px] xl:w-[100px] active:bg-sky-900"
+                        >
+                            Buscar
+                        </button>
+                    </div>
                 </form>
-                {climaToday && (
-                    <div className="border bg-sky-500 w-5/6">
-                        <h2>Today:</h2>
-                        <p>Temperatura atual: {climaToday.main.temp}°C</p>
-                        <p>Temperatura Máxima: {climaToday.main.temp_max}°C</p>
-                        <p>Temperatura Mínima: {climaToday.main.temp_min}°C</p>
-                    </div>
-                )}
-                {climaData && (
-                    <div>
-                        <h2 className="text-3xl font-bold underline">
-                            Resultados encontrados:
-                        </h2>
-                        <ul>
-                            {climaData.list.map((item, index) => (
-                                <li key={index}>
-                                    <p>
-                                        Data: {formatData(item.dt)}, Hora:{" "}
-                                        {formatHours(item.dt)}, Temperatura Máx:{" "}
-                                        {item.main.temp_max}°C, Temperatura mín:{" "}
-                                        {item.main.temp_min}°C, Umidade:{" "}
-                                        {item.main.humidity}
-                                    </p>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
+                <div
+                    className={
+                        climaData && climaToday
+                            ? "bg-black/25 w-11/12 md:w-4/6 p-4 xl:p-6 rounded-lg"
+                            : ""
+                    }
+                >
+                    {climaToday && (
+                        <div className="border bg-slate-200 text-black rounded-lg flex flex-col items-center justify-center gap-7 py-5 md:h-[200px]">
+                            <p className="text-3xl md:text-7xl font-black">
+                                {" "}
+                                {Math.round(climaToday.main.temp)}°C
+                            </p>
+                            <div className="text-sm md:text-lg font-bold text-slate-500">
+                                <p>
+                                    Min {Math.round(climaToday.main.temp) + 6}
+                                    °C / Max{" "}
+                                    {Math.round(climaToday.main.temp) - 6}
+                                    °C
+                                </p>
+                            </div>
+                        </div>
+                    )}
+                    {climaData && (
+                        <div className="">
+                            <ul className="mt-5 flex gap-[20px] rounded-lg flex-wrap w-100">
+                                {climaData.map((item, index) => (
+                                    <li
+                                        key={index}
+                                        className="bg-gray-400 text-white flex flex-row justify-between items-center gap-3 px-2 md:px-5 flex-1 w-full md:w-2/6 h-[100px] md:h-[130px] lg:h-[150px] rounded-lg"
+                                    >
+                                        <p className="md:text-2xl">
+                                            {" "}
+                                            {new Date(
+                                                item.dt * 1000
+                                            ).toLocaleDateString("pt-br", {
+                                                day: "numeric",
+                                                month: "numeric",
+                                            })}
+                                        </p>
+                                        <div className="flex flex-col justify-around h-full font-bold text-2xl md:text-5xl">
+                                            <p>
+                                                {Math.round(item.main.temp) + 6}
+                                                °
+                                            </p>
+                                            <p>
+                                                {Math.round(item.main.temp) - 6}
+                                                °
+                                            </p>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
